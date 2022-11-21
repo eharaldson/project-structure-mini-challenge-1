c6a3a3ff911e00b59f68cd9640c9c871b8340634
Erik Haraldson <erikharaldson@MacBook-Pro-9.local>
2022-11-21 12:08:26 +0000

    Adds tests and requirements file

--- test_date.py
+++ test_date.py
@@ -4,10 +4,14 @@ from celebrities_births import Date
 class DateTest(unittest.TestCase):
     
     def setUp(self):
-        self.new_date = Date(32, 10, 2021)
+        self.new_date = Date(30, 10, 2021)
 
     def test_date_valid(self):
-        self.assertRaises(ValueError)
+        assert self.new_date.is_date_valid(32, 10, 2021) == False
 
+    def test_from_string(self):
+        date = '12-10-2021'
+        expected_value = Date(12, 10, 2021)
+        assert Date.from_string(date) == expected_value
 
-t=(1)
+unittest.main(argv=[''], verbosity=2, exit=False)
